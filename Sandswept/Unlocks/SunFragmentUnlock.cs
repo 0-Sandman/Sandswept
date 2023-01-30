308b68213634ab7de12fa15d4acb271ce4e8c5d1
Sandman <CA.TurnerWA@gmail.com>
2023-01-30 08:10:55 +0800

    Unlock implementation for SFrag

---
+++ Sandswept/Unlocks/SunFragmentUnlock.cs
@@ -0,0 +1,28 @@
+﻿using RoR2.Achievements;
+using RoR2;
+using BepInEx.Configuration;
+
+namespace Sandswept.Unlocks
+{
+    [RegisterAchievement("SUN_FRAGMENT", "ITEM_SUN_FRAGMENT", null, null)]
+    public class Tracker : BaseAchievement
+    {
+        public override void OnInstall()
+        {
+            base.OnInstall();
+            On.RoR2.ScrapperController.BeginScrapping += ScrapperController_BeginScrapping;
+        }
+
+        private void ScrapperController_BeginScrapping(On.RoR2.ScrapperController.orig_BeginScrapping orig, ScrapperController self, int intPickupIndex)
+        {
+            if (self.interactor != null)
+            {
+                if (intPickupIndex == 146)
+                {
+                    Grant();
+                }
+            }
+            orig(self, intPickupIndex);
+        }
+    }
+}
