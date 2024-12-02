719a02d90826d1b865f152fd1e422c1aba215216
psuedopulse <psuedopulse@protonmail.com>
2024-12-02 14:22:39 -0600

    joe biden

---
+++ Sandswept/Enemies/ThetaConstruct/CastShield.cs
@@ -0,0 +1,74 @@
+using System;
+using RoR2.CharacterAI;
+using Sandswept.Survivors;
+
+namespace Sandswept.Enemies.ThetaConstruct {
+    public class CastShield : BaseSkillState {
+        public GameObject shieldInstance;
+        public float duration = 15f;
+        public override void OnEnter()
+        {
+            base.OnEnter();
+
+            GameObject buddy = base.characterBody.master.GetComponent<BaseAI>().buddy._gameObject;
+
+            if (!buddy) {
+                outer.SetNextStateToMain();
+                return;
+            }
+
+            if (base.isAuthority) {
+                shieldInstance = GameObject.Instantiate(ThetaConstruct.ThetaShieldEffect, buddy.transform.position, Quaternion.identity);
+                ThetaShieldController shieldController = shieldInstance.GetComponent<ThetaShieldController>();
+                shieldController.targetHolder.ownerObject = buddy;
+                shieldController.ownerHolder.ownerObject = base.gameObject;
+
+                base.characterBody.master.GetComponent<BaseAI>().leader.gameObject = buddy;
+
+                NetworkServer.Spawn(shieldInstance);
+            }
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            if (!base.isAuthority) return;
+
+            if (base.fixedAge >= duration || !shieldInstance) {
+                outer.SetNextStateToMain();
+            }
+        }
+
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.Frozen;
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+
+            if (base.isAuthority && shieldInstance) {
+                NetworkServer.DestroyObject(shieldInstance);
+            }
+        }
+    }
+
+    public class CastShieldSkill : SkillBase<CastShieldSkill>
+    {
+        public override string Name => "";
+
+        public override string Description => "";
+
+        public override Type ActivationStateType => typeof(CastShield);
+
+        public override string ActivationMachineName => "Weapon";
+
+        public override float Cooldown => 10f;
+
+        public override Sprite Icon => null;
+        public override bool BeginCooldownOnSkillEnd => true;
+        public override bool CanceledFromSprinting => false;
+    }
+}
---
+++ Sandswept/Enemies/ThetaConstruct/ThetaConstruct.cs
@@ -0,0 +1,148 @@
+using System;
+
+namespace Sandswept.Enemies.ThetaConstruct {
+    public class ThetaConstruct : EnemyBase<ThetaConstruct>
+    {
+        public static GameObject ThetaShieldEffect;
+        public override void LoadPrefabs()
+        {
+            prefab = Main.Assets.LoadAsset<GameObject>("ThetaConstructBody.prefab");
+            prefabMaster = Main.Assets.LoadAsset<GameObject>("ThetaConstructMaster.prefab");
+
+            ThetaShieldEffect = Main.Assets.LoadAsset<GameObject>("ThetaShieldEffect.prefab");
+            ThetaShieldEffect.GetComponent<MeshRenderer>().sharedMaterial = Paths.Material.matMinorConstructShield;
+        }
+
+        public override void PostCreation()
+        {
+            base.PostCreation();
+            RegisterEnemy(prefab, prefabMaster, null);
+        }
+
+        public override void Modify()
+        {
+            base.Modify();
+
+            body.baseNameToken.Add("Theta Construct");
+
+            SkillLocator loc = body.GetComponent<SkillLocator>();
+            
+            ReplaceSkill(loc.primary, CastShieldSkill.instance.skillDef);
+        }
+    }
+
+    public class ThetaShieldController : MonoBehaviour
+    {
+        public GenericOwnership targetHolder;
+        public GenericOwnership ownerHolder;
+        private GameObject owner;
+        private GameObject target;
+        private CharacterBody ownerBody;
+        private CharacterBody targetBody;
+        private Animator anim;
+        private Transform p1, p1t, p2, p2t, p3, p3t;
+        private Vector3 p1c, p2c, p3c = Vector3.zero;
+        private Vector3 p1r, p2r, p3r = Vector3.zero;
+        private List<BuffIndex> activeEliteBuffs = new();
+        public void Start()
+        {
+            owner = ownerHolder.ownerObject;
+            target = targetHolder.ownerObject;
+
+            var loc = GetComponent<ChildLocator>();
+
+            p1t = loc.FindChild("PrismL");
+            p2t = loc.FindChild("PrismR");
+            p3t = loc.FindChild("PrismT");
+
+            if (owner)
+            {
+                ownerBody = owner.GetComponent<CharacterBody>();
+
+                var loc2 = ownerBody.GetComponent<ModelLocator>().modelTransform.GetComponent<ChildLocator>();
+
+                p1 = loc2.FindChild("PrismL");
+                p2 = loc2.FindChild("PrismR");
+                p3 = loc2.FindChild("PrismT");
+            }
+
+            if (target)
+            {
+                targetBody = target.GetComponent<CharacterBody>();
+                base.transform.localScale *= targetBody.bestFitRadius * 1.5f;
+
+                if (NetworkServer.active)
+                {
+                    if (ownerBody && ownerBody.isElite)
+                    {
+                        for (int i = 0; i < ownerBody.activeBuffsList.Length; i++)
+                        {
+                            var buff = ownerBody.activeBuffsList[i];
+
+                            BuffDef bd = BuffCatalog.GetBuffDef(buff);
+
+                            if (bd.isElite && !targetBody.HasBuff(buff))
+                            {
+                                activeEliteBuffs.Add(buff);
+                                targetBody.SetBuffCount(buff, 1);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!owner || !target)
+            {
+                Destroy(base.gameObject);
+            }
+        }
+
+        public void LateUpdate()
+        {
+            if (owner)
+            {
+                Handle(ref p1, ref p1t, ref p1c, ref p1r);
+                Handle(ref p2, ref p2t, ref p2c, ref p2r);
+                Handle(ref p3, ref p3t, ref p3c, ref p3r);
+
+                void Handle(ref Transform p, ref Transform t, ref Vector3 c, ref Vector3 r) {
+                    if (c == Vector3.zero) c = p.position;
+                    p.position = c;
+                    p.position = Vector3.MoveTowards(p.position, t.position, 40f * Time.fixedDeltaTime);
+                    c = p.position;
+                    if (r == Vector3.zero) r = p.up;
+                    p.up = r;
+                    p.up = Vector3.RotateTowards(p.up, t.up, 40f * Time.fixedDeltaTime, 1f);
+                    r = p.up;
+                }
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (target)
+            {
+                base.transform.position = Vector3.MoveTowards(base.transform.position, targetBody.corePosition, 90f * Time.fixedDeltaTime);
+            }
+
+            if (!owner || !target)
+            {
+                Destroy(base.gameObject);
+            }
+        }
+
+        public void OnDestroy()
+        {
+            if (targetBody)
+            {
+                if (activeEliteBuffs.Count > 0)
+                {
+                    foreach (BuffIndex buff in activeEliteBuffs)
+                    {
+                        targetBody.SetBuffCount(buff, 0);
+                    }
+                }
+            }
+        }
+    }
+}
