b4e43d98d98a3fff8150a3832968317c6d85f499
hifoomin <jakubrataj@protonmail.com>
2025-01-08 08:13:43 +0100

    was supposed to impl the other white out of boredom but improved trans vfx instead

--- Sandswept/Items/Whites/TemporalTransistor.cs
+++ Sandswept/Items/Whites/TemporalTransistor.cs
@@ -1,4 +1,7 @@
-﻿namespace Sandswept.Items.Whites
+﻿using RoR2.Orbs;
+using System.Reflection;
+
+namespace Sandswept.Items.Whites
 {
     [ConfigSection("Items :: Temporal Transistor")]
     internal class TemporalTransistor : ItemBase<TemporalTransistor>
@@ -21,7 +24,7 @@
 
         public override ItemTag[] ItemTags => new ItemTag[] { ItemTag.OnKillEffect, ItemTag.Utility };
 
-        [ConfigField("Chance", "", 15f)]
+        [ConfigField("Chance", "", 20f)]
         public static float chance;
 
         [ConfigField("Base Max Jumps", "", 2)]
@@ -31,6 +34,8 @@
         public static int stackMaxJumps;
 
         public static BuffDef extraJump;
+
+        public static GameObject orbEffect;
 
         public override void Init(ConfigFile config)
         {
@@ -43,6 +48,65 @@
             extraJump.iconSprite = Main.hifuSandswept.LoadAsset<Sprite>("texBuffTemporalTransistor.png");
             extraJump.name = "Temporal Transistor - Extra Jump";
             ContentAddition.AddBuffDef(extraJump);
+
+            var blue = new Color32(102, 195, 255, 255);
+            var darkerBlue = new Color32(102, 148, 255, 255);
+            var aqua = new Color32(169, 249, 255, 127);
+
+            TemporalTransistor.orbEffect = PrefabAPI.InstantiateClone(Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Infusion/InfusionOrbEffect.prefab").WaitForCompletion(), "Temporal Transistor Orb", false);
+
+            var orbEffect = TemporalTransistor.orbEffect.GetComponent<OrbEffect>();
+            orbEffect.movementCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.33f, 0f), new Keyframe(1f, 1f));
+            GameObject.DestroyImmediate(orbEffect.GetComponent<AkEvent>());
+            GameObject.DestroyImmediate(orbEffect.GetComponent<AkTriggerDisable>());
+
+            var trail = TemporalTransistor.orbEffect.transform.Find("TrailParent/Trail").GetComponent<TrailRenderer>();
+
+            var newTrailMaterial = new Material(Paths.Material.matInfusionTrail);
+            newTrailMaterial.SetTexture("_RemapTex", Paths.Texture2D.texRampTritone);
+            newTrailMaterial.SetColor("_TintColor", blue);
+            newTrailMaterial.SetColor("_CutoffScroll", new Color32(75, 0, 12, 12));
+
+            trail.material = newTrailMaterial;
+            trail.time = 1.5f;
+
+            var vfx = TemporalTransistor.orbEffect.transform.Find("VFX");
+            var vfxRotateItem = vfx.GetComponent<RotateItem>();
+            vfxRotateItem.spinSpeed = 60f;
+            vfxRotateItem.bobHeight = 1.5f;
+
+            var core = vfx.Find("Core");
+            var coreColorOverLifetime = core.GetComponent<ParticleSystem>().colorOverLifetime;
+
+            var coreColors = new GradientColorKey[3];
+            coreColors[0] = new GradientColorKey(blue, 0f);
+            coreColors[1] = new GradientColorKey(blue, 0.75f);
+            coreColors[2] = new GradientColorKey(darkerBlue, 0f);
+
+            var coreAlphas = new GradientAlphaKey[3];
+            coreAlphas[0] = new GradientAlphaKey(1f, 0f);
+            coreAlphas[1] = new GradientAlphaKey(1f, 0.75f);
+            coreAlphas[2] = new GradientAlphaKey(0f, 0f);
+
+            var newCoreColorGradient = new Gradient();
+            newCoreColorGradient.SetKeys(coreColors, coreAlphas);
+
+            coreColorOverLifetime.color = newCoreColorGradient;
+
+            var coreParticleSystemRenderer = core.GetComponent<ParticleSystemRenderer>();
+
+            var newCoreMaterial = Object.Instantiate(Paths.Material.matExpTrail);
+            newCoreMaterial.SetTexture("_BaseTex", Paths.Texture2D.texArtifactCompoundDiamondMask);
+
+            coreParticleSystemRenderer.material = newCoreMaterial;
+
+            var pulseGlowMain = vfx.Find("PulseGlow").GetComponent<ParticleSystem>().main;
+            var pulseGlowStartColor = pulseGlowMain.startColor;
+            pulseGlowStartColor.mode = ParticleSystemGradientMode.Color;
+            pulseGlowStartColor.color = aqua;
+
+            ContentAddition.AddEffect(TemporalTransistor.orbEffect);
+
             CreateLang();
             CreateItem();
             Hooks();
@@ -52,12 +116,35 @@
         {
             GlobalEventManager.onCharacterDeathGlobal += GlobalEventManager_onCharacterDeathGlobal;
             On.EntityStates.GenericCharacterMain.ProcessJump += GenericCharacterMain_ProcessJump;
+            On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
+        }
+
+        private void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
+        {
+            orig(self);
+
+            var storedJumps = self.GetBuffCount(extraJump);
+            self.maxJumpCount += storedJumps;
+
+            /*
+            if (self.characterMotor.jumpCount == self.maxJumpCount && storedJumps >= 1)
+            {
+                self.SetBuffCount(extraJump.buffIndex, self.GetBuffCount(extraJump) - 1);
+                self.maxJumpCount += 1;
+            }
+            */
         }
 
         private void GlobalEventManager_onCharacterDeathGlobal(DamageReport report)
         {
             var attackerBody = report.attackerBody;
             if (!attackerBody)
+            {
+                return;
+            }
+
+            var victimBody = report.victimBody;
+            if (!victimBody)
             {
                 return;
             }
@@ -71,10 +158,13 @@
             var stack = GetCount(attackerBody);
             if (stack > 0 && Util.CheckRoll(chance, masterrrrPleadingMasterrr))
             {
-                var storedJumps = attackerBody.GetBuffCount(extraJump);
-                var maxStoredJumps = baseMaxJumps + stackMaxJumps * (stack - 1);
+                var temporalTransistorOrb = new TemporalTransistorOrb
+                {
+                    origin = victimBody.corePosition,
+                    target = Util.FindBodyMainHurtBox(attackerBody)
+                };
 
-                attackerBody.SetBuffCount(extraJump.buffIndex, Mathf.Min(storedJumps + 1, maxStoredJumps));
+                OrbManager.instance.AddOrb(temporalTransistorOrb);
             }
         }
 
@@ -88,16 +178,11 @@
 
             var storedJumps = self.characterBody.GetBuffCount(extraJump);
 
-            if (self.characterMotor.jumpCount == self.characterBody.maxJumpCount && storedJumps >= 1)
+            if (self.characterMotor.jumpCount > self.characterBody.maxJumpCount - storedJumps)
             {
-                var jumpCount = self.characterBody.maxJumpCount;
-
                 self.characterBody.SetBuffCount(extraJump.buffIndex, self.characterBody.GetBuffCount(extraJump) - 1);
-                self.characterBody.maxJumpCount += 1;
 
                 orig(self);
-
-                self.characterBody.maxJumpCount = jumpCount;
             }
             else
             {
@@ -108,6 +193,49 @@
         public override ItemDisplayRuleDict CreateItemDisplayRules()
         {
             return new ItemDisplayRuleDict();
+        }
+    }
+
+    public class TemporalTransistorOrb : Orb
+    {
+        public CharacterBody body;
+        public Inventory inventory;
+
+        public override void Begin()
+        {
+            duration = distanceToTarget / 50f;
+            var effectData = new EffectData()
+            {
+                origin = origin,
+                genericFloat = duration
+            };
+            effectData.SetHurtBoxReference(target);
+            EffectManager.SpawnEffect(TemporalTransistor.orbEffect, effectData, true);
+            var hurtBox = target.GetComponent<HurtBox>();
+            var hc = hurtBox.healthComponent;
+            if (hc)
+            {
+                body = hc.GetComponent<CharacterBody>();
+                inventory = body.inventory;
+            }
+        }
+
+        public override void OnArrival()
+        {
+            if (body && inventory)
+            {
+                var stack = inventory.GetItemCount(TemporalTransistor.instance.ItemDef);
+                var storedJumps = body.GetBuffCount(TemporalTransistor.extraJump);
+                var maxStoredJumps = TemporalTransistor.baseMaxJumps + TemporalTransistor.stackMaxJumps * (stack - 1);
+
+                body.SetBuffCount(TemporalTransistor.extraJump.buffIndex, Mathf.Min(storedJumps + 1, maxStoredJumps));
+
+                for (int i = 0; i < 2; i++)
+                {
+                    Util.PlayAttackSpeedSound("Play_mage_m1_cast_lightning", body.gameObject, 2f);
+                    Util.PlayAttackSpeedSound("Play_mage_m1_cast_lightning", body.gameObject, 1.5f);
+                }
+            }
         }
     }
 }
